6b66cdb0a557b4f51a62f32c2ce5b2f51d5eb226
plamen <plamen.ivanov17@gmail.com>
2018-06-28 14:59:09 +0300

    Added the article-box component fade in animation

--- app/components/common/article-box/article-box.js
+++ app/components/common/article-box/article-box.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import {CSSTransition} from "react-transition-group";
 import moment from "moment";
 
 import Utils from "../../../services/utils";
@@ -20,39 +21,40 @@ class ArticleBox extends React.Component {
 	}
 		
 	render() {
+				
 		return (
-			<div className="article-box">
-	
-				<Link to={"/article/"+this.props.article.id} className="image-wrapper">
-					<div className="section-bar">
-						<span className="section">
-							{this.props.article.categoryName}
-						</span>
-						<span className="date">
-							{moment(this.props.article.date).format("YYYY-MM-DD")}
-						</span>
-					</div>
-					<img className="img-fluid" src={this.imagesDir+this.props.article.image}/>
-				</Link>
-				
-				<div className="content-wrapper">
-					<div className="title">
-						<Link to={"/article/"+this.props.article.id}>
-							{this.props.article.title}
-						</Link>
-					</div>
-					<div className="summary">
-						{Utils.limitTo(this.props.article.summary, 150)}
-					</div>
-					
-					<Link to={"/article/"+this.props.article.id} className="btn btn-primary-light">
-						Read more
+			<CSSTransition in appear={true} timeout={300} classNames="fade">
+				<div className="article-box">
+					<Link to={"/article/"+this.props.article.id} className="image-wrapper">
+						<div className="section-bar">
+							<span className="section">
+								{this.props.article.categoryName}
+							</span>
+							<span className="date">
+								{moment(this.props.article.date).format("YYYY-MM-DD")}
+							</span>
+						</div>
+						<img className="img-fluid" src={this.imagesDir+this.props.article.image}/>
 					</Link>
-					
-					<div className="clearfix"></div>
+
+					<div className="content-wrapper">
+						<div className="title">
+							<Link to={"/article/"+this.props.article.id}>
+								{this.props.article.title}
+							</Link>
+						</div>
+						<div className="summary">
+							{Utils.limitTo(this.props.article.summary, 150)}
+						</div>
+
+						<Link to={"/article/"+this.props.article.id} className="btn btn-primary-light">
+							Read more
+						</Link>
+
+						<div className="clearfix"></div>
+					</div>
 				</div>
-				
-			</div>
+			</CSSTransition>
 		);
 	}
 };
