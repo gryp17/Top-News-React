658be8020269f000553e2f5d6b1c56cb6d2f91c3
plamen <plamen.ivanov17@gmail.com>
2018-07-18 16:40:33 +0300

    Implemented most the articles slider logic

--- app/components/pages/article/articles-slider/articles-slider.js
+++ app/components/pages/article/articles-slider/articles-slider.js
@@ -17,31 +17,118 @@ class ArticlesSlider extends React.Component {
 		super(props);
 		
 		this.articlesDir = Config.articlesDir;
+		this.maxVisibleSlides = 3;
+		
+		this.state = {
+			margin: 0
+		};
+		
+		this.getDimensions = this.getDimensions.bind(this);
+		this.setSliderWidth = this.setSliderWidth.bind(this);
+		this.handleWheelScroll = this.handleWheelScroll.bind(this);
+		this.slideLeft = this.slideLeft.bind(this);
+		this.slideRight = this.slideRight.bind(this);
+		this.slide = this.slide.bind(this);
+	}
+	
+	componentDidMount(){
+		$(window).on("resize", this.setSliderWidth);
+		$(this.refs.wrapper).on("mousewheel DOMMouseScroll", this.handleWheelScroll);
+	}
+	
+	componentWillUnmount(){
+		$(window).off("resize", this.setSliderWidth);
+		$(this.refs.wrapper).off("mousewheel DOMMouseScroll", this.handleWheelScroll);
+	}
+	
+	/**
+	 * Returns the most needed component dimmensions (wrapper, slider and slide width)
+	 */
+	getDimensions(){
+		return {
+			wrapperWidth: $(this.refs.wrapper).width(),
+			sliderWidth: $(this.refs.slider).width(),
+			slideWidth: $(this.refs.wrapper).width() / this.maxVisibleSlides //3 visible articles at all times
+		};
+	}
+	
+	/**
+	 * Sets the slider width based on the articles count
+	 */
+	setSliderWidth(){
+		var dimensions = this.getDimensions();
+		$(this.refs.slider).width(this.props.articles.length * dimensions.slideWidth);
+	}
+	
+	/**
+	 * Listens for the mouse wheel event and slides the slider left or right
+	 * @param {Object} e
+	 */
+	handleWheelScroll(e){
+		e.preventDefault();
+		
+		if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+			this.slideLeft();
+		} else {
+			this.slideRight();
+		}
+	}
+	
+	/**
+	 * Makes the slider slide left
+	 */
+	slideLeft(){
+		var dimensions = this.getDimensions();
+		var maxMargin = 0;
+		
+		var newMargin = this.state.margin + dimensions.slideWidth;
+		
+		if(newMargin > maxMargin){
+			return;
+		}
+		
+		this.slide(newMargin);
+	}
+	
+	/**
+	 * Makes the slider slide right
+	 */
+	slideRight(){
+		var dimensions = this.getDimensions();
+		var minMargin = (dimensions.sliderWidth - dimensions.wrapperWidth + 10) * -1;
+		
+		var newMargin = this.state.margin - dimensions.slideWidth;
+
+		if(newMargin < minMargin){
+			return;
+		}
+		
+		this.slide(newMargin);
+	}
+	
+	/**
+	 * Makes the slider slide to the given margin
+	 * @param {Number} margin
+	 */
+	slide(margin){
+		this.setState({
+			margin: margin
+		});
+		
+		$(this.refs.slider).stop().animate({marginLeft: this.state.margin}, 500);
 	}
 	
 	render() {
 		var self = this;
 		
-		//TODO:
-		//add mouse scroll event for moving the slider left or right
-		
-		var width = $(this.refs.wrapper).width();
-		
-		//set the slider size based on the articles count
-		$(this.refs.slider).width(this.props.articles.length * 220);
-		
-		//maybe add an interval that rotates the slides like this?
-		//on window resize - reset all margins and start from the first slide again
-		setTimeout(function (){
-			$(self.refs.slider).animate({marginLeft: -220}, 500);
-		}, 3000);
-		
+		this.setSliderWidth();
+				
 		var articles = this.props.articles.map(function (article){
 			return (
-				<div className="slide" key={article.id}>
+				<div className="slide col" key={article.id}>
 					<img className="img-fluid image" src={self.articlesDir + article.image}/>
-					<div className="summary">
-						{article.summary}
+					<div className="title">
+						{article.title}
 					</div>
 				</div>
 			);
@@ -49,7 +136,7 @@ class ArticlesSlider extends React.Component {
 		
 		return (
 			<div className="articles-slider" ref="wrapper">
-				<div className="slider" ref="slider">
+				<div className="slider row" ref="slider">
 					{articles}
 				</div>
 			</div>
