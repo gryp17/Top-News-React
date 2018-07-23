65ec1aeaabd1ba4cada888b0f17d812da5361c01
plamen <plamen.ivanov17@gmail.com>
2018-07-23 13:54:29 +0300

    Added the "no-results" component to the search page

---
+++ app/components/pages/search/no-results/no-results.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+import FadeIn from "../../../common/animations/fade-in/fade-in";
+
+import "./no-results.scss";
+
+class NoResults extends React.Component {
+	render() {
+		return (
+			<FadeIn>
+				<div className="no-results">
+					<div className="title">
+						No results were found for this search.
+					</div>
+					
+					<img src="/img/icons/warning-icon.png"/>
+					
+					<div className="home-link">
+						Go back to the <Link to="/">home page</Link>
+					</div>
+				</div>
+			</FadeIn>
+		);
+	}
+};
+
+export default NoResults;
--- app/components/pages/search/search-page.js
+++ app/components/pages/search/search-page.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./search-page.scss";
 
 import ArticleHttpService from "../../../services/api/article";
+import NoResults from "./no-results/no-results";
 import ArticleBox from "../../common/article-box/article-box";
 import LoadingIndicator from "../../common/loading-indicator/loading-indicator";
 import ScrollToTop from "../../common/scroll-to-top/scroll-to-top";
@@ -116,26 +117,21 @@ class SearchPage extends React.Component {
 	
 	render() {
 		
-		var content;
-		
-		if(this.state.loading){
-			content = <LoadingIndicator/>;
-		}else{
-			var articles = this.state.articles.map(function (article){
-				return (
-					<div className="col-12 col-sm-6" key={article.id}>
-						<ArticleBox article={article}/>
-					</div>
-				);
-			});
-			
-			content = <div className="row no-gutters articles-wrapper">{articles}</div>;
-		}
+		var articles = this.state.articles.map(function (article){
+			return (
+				<div className="col-12 col-sm-6" key={article.id}>
+					<ArticleBox article={article}/>
+				</div>
+			);
+		});
 		
 		return (
-			<div id="search-page">
-				{content}
-						
+			<div id="search-page">	
+
+				{this.state.loading && <LoadingIndicator/>}
+				{!this.state.loading && articles.length === 0 && <NoResults/>}
+				{!this.state.loading && articles.length > 0 && <div className="row no-gutters articles-wrapper">{articles}</div>}
+	
 				<ScrollToTop/>
 			</div>
 		);
