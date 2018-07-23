b0d4faa765353982e685a98d25e58cc2f5e514ba
plamen <plamen.ivanov17@gmail.com>
2018-07-23 12:00:03 +0300

    Changed the way the autocomplete options are generated and added the text-overflow effect

--- api/routes/article.js
+++ api/routes/article.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var striptags = require("striptags");
 var router = express.Router();
 
 var ArticleModel = require("../models/article");
@@ -28,7 +27,7 @@ router.get("/category/:category/:searchTerm/:limit/:offset", function (req, res,
 
 //get all article search suggestions from the specified category and search term
 router.get("/autocomplete/:category/:searchTerm/:limit", function (req, res, next){
-	var textLimit = 40;
+	var textLimit = 80;
 	
 	ArticleModel.search(req.params.category, req.params.searchTerm, parseInt(req.params.limit), 0, function (err, results) {
 		if (err) {
@@ -39,16 +38,20 @@ router.get("/autocomplete/:category/:searchTerm/:limit", function (req, res, nex
 		results = results.map(function (article){
 			var result = "";
 			var title = article.title;
-			var content = striptags(article.content, [], " ");
+			var content = article.content;
 			
 			var titleIndex = title.toLowerCase().indexOf(req.params.searchTerm.toLowerCase());
 			var contentIndex = content.toLowerCase().indexOf(req.params.searchTerm.toLowerCase());
 			
 			if(titleIndex !== -1){
 				result = title.substr(titleIndex, req.params.searchTerm.length + textLimit);
-			}else if(contentIndex !== -1){
+			}else if(contentIndex !== -1){				
 				result = content.substr(contentIndex, req.params.searchTerm.length + textLimit);
 			}
+
+			//remove any HTML tags and dots that might appear at the end of the string
+			result = result.replace(/<\w+.+/ig, "");
+			result = result.replace(/\.$/, "");
 
 			return result;
 		});
