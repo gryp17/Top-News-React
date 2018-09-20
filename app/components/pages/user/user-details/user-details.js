9a98ca04cdaf4bdb2acd2abc177d040d2c686ad5
plamen <plamen.ivanov17@gmail.com>
2018-09-20 15:42:11 +0300

    Finished the user details component

--- app/components/pages/user/user-details/user-details.js
+++ app/components/pages/user/user-details/user-details.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import moment from "moment";
 
 import "./user-details.scss";
 
@@ -9,39 +10,34 @@ var UserDetails = function (props) {
 	
 	var user = props.user;
 	var avatarsDir = Config.avatarsDir;
+	var registered = moment(user.registered);
+	
 	
 	return (
 		<div className="user-details">
 			<h4>
 				User Profile For <strong>{props.user.username}</strong>
+				{(props.currentUser && props.currentUser.id === user.id) && <button className="btn btn-success edit-btn">Edit</button>}
 			</h4>
 			
 			<hr/>
 			
 			<div className="row no-gutters">
-				<div className="col-md-3">
+				<div className="col-md-5 avatar-wrapper">
 					<img src={avatarsDir+user.avatar}/>
 				</div>
-				<div className="col-md-9">
-					<div>
-						Username: {user.username}
+				<div className="col-md-7 info-wrapper">
+					<div className="item">
+						<strong>Username:</strong> {user.username}
 					</div>
-
-					<div>
-						Account type: {user.type}
+					<div className="item">
+						<strong>Account Type:</strong> {user.type}
 					</div>
-
-					<div>
-						Active: {user.active}
-					</div>
-
-					<div>
-						Registered: {user.registered}
+					<div className="item">
+						<strong>Registered:</strong> <span title={registered.format("YYYY-MM-DD hh:mm:ss")}>{registered.from(moment())}</span>
 					</div>
 				</div>
 			</div>
-			
-			{(props.currentUser && props.currentUser.id === user.id) && <button className="btn btn-success">Edit</button>}
 			
 		</div>
 	);
