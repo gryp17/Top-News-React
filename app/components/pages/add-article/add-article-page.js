f962ef6a5c63e6894edd5d1a8b48e9b1cad109d2
plamen <plamen.ivanov17@gmail.com>
2018-10-11 12:27:48 +0300

    Added the basic layout of the "add article" page

--- app/components/pages/add-article/add-article-page.js
+++ app/components/pages/add-article/add-article-page.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
+import _ from "lodash";
 
 import "./add-article-page.scss";
 
@@ -20,8 +22,49 @@ class AddArticlePage extends React.Component {
 		super(props);
 
 		this.state = {
+			article: {
+				categoryId: 1,
+				title: "",
+				summary: "",
+				content: ""
+			},
+			errors: {},
 			loading: false
 		};
+
+		this.sections = {
+			1: "Politics",
+			2: "Economy",
+			3: "World",
+			4: "Technology",
+			5: "Sports"
+		}
+
+		this.handleChange = this.handleChange.bind(this);
+		this.saveArticle = this.saveArticle.bind(this);
+	}
+
+	generateSections(){
+		var options = [];
+
+		_.forOwn(this.sections, function (value, key){
+			options.push(<option key={key} value={key}>{value}</option>);
+		});
+
+		return options;
+	}
+
+	handleChange(e){
+		var article = {...this.state.article};
+		article[e.target.name] = e.target.value;
+
+		this.setState({
+			article: article
+		});
+	}
+
+	saveArticle(){
+
 	}
 	
 	render() {
@@ -32,8 +75,70 @@ class AddArticlePage extends React.Component {
 		return (
 			<div id="add-article-page">
 
-                {!isAdmin && <Forbidden/>}
-                {isAdmin && <div>add article page</div>}
+				{!isAdmin && <Forbidden/>}
+				
+                {isAdmin && 
+					<form ref="addArticleForm">
+
+						<h4>Add Article</h4>
+
+						<hr/>
+
+						<div className="form-group">
+							<label>Section</label>
+							<select name="categoryId" value={this.state.article.categoryId} onChange={this.handleChange}
+								className="form-control">
+
+								{this.generateSections()}
+
+							</select>
+						</div>
+
+						<div className="form-group">
+							<label>Title</label>
+							<textarea name="title" value={this.state.article.title} onChange={this.handleChange}
+								className={classNames("form-control title", {"is-invalid": this.state.errors.title})}
+								placeholder="Title"
+								onFocus={this.clearErrors}></textarea>
+
+							<div className="form-error">
+								{this.state.errors.title}
+							</div>
+						</div>
+
+						<div className="form-group">
+							<label>Summary</label>
+							<textarea name="summary" value={this.state.article.summary} onChange={this.handleChange}
+								rows="3"
+								className={classNames("form-control summary", {"is-invalid": this.state.errors.summary})}
+								placeholder="Summary"
+								onFocus={this.clearErrors}></textarea>
+
+							<div className="form-error">
+								{this.state.errors.summary}
+							</div>
+						</div>
+
+						<div className="form-group">
+							<label>Content</label>
+							<textarea name="content" value={this.state.article.content} onChange={this.handleChange}
+								rows="20"
+								className={classNames("form-control summary", {"is-invalid": this.state.errors.content})}
+								placeholder="Content"
+								onFocus={this.clearErrors}></textarea>
+
+							<div className="form-error">
+								{this.state.errors.content}
+							</div>
+						</div>
+
+
+						<button type="button" className="btn btn-success submit-btn" onClick={this.saveArticle}>
+							Submit
+						</button>
+
+					</form>
+				}
 
 			</div>
 		);
